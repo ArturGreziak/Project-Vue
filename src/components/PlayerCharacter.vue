a14c5ace72d9b1f964fad4570c879dfdc285aa31
Artur Gręziak <alex4448@wp.pl>
2021-07-11 15:01:32 +0200

    first commit

---
+++ src/components/PlayerCharacter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="panel panel-default">
+        
+        <div class="panel-heading">
+            <PlayerCharacterName/>
+        </div>
+        <div class="panel-body">
+          
+            <PlayerCharacterHealthbar/>
+        </div>
+    </div>
+</template>
+  
+<script>
+  import PlayerCharacterName from './PlayerCharacterName.vue'
+  import PlayerCharacterHealthbar from './PlayerCharacterHealthbar.vue'
+ export default {
+   compoents: {
+     PlayerCharacterName,
+     PlayerCharacterHealthbar
+   }
+ }
+</script>
+
+<style>
+  
+</style>
---
+++ src/components/PlayerCharacterName.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <button type="button" class="btn btn-success">Success</button>
+  <h3 class='panel-title'>Gracz</h3>
+</div>
+    
+</template>
+
+<script>
+ export default {
+   
+ }
+</script>
+
+<style>
+ 
+</style>
